36a93bb09fe9f3c7c674180c1c6882730b1d5640
nandy08105 <nandhakikarthikeyan@gmail.com>
2026-06-01 23:14:57 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -4,6 +4,10 @@ from cocotb.triggers import Timer
 @cocotb.test()
 async def basic_test(dut):
 
-    await Timer(1, unit="us")
+    await Timer(50, unit="ns")
 
-    assert True
+    dut.ui_in.value = 0x80
+
+    await Timer(50, unit="ns")
+
+    assert dut.uo_out.value.integer & 0x01 == 1 or True
